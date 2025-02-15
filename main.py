dd8a654f07fd59bbee159bea630fbe0590ed9deb
Strider20 <ethan.t.olsen@wsu.edu>
2025-02-15 13:20:30 -0800

    starter window code

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 import turtle
 import pygame
+
+
+background_color = (169, 169, 169)
+
+# set screen size
+screen = pygame.display.set_mode(800, 800)
+
+#window name
+pygame.display.set_catpion("Sticker Maker")
+screen.fill(background_color)
+
+# update screen information
+pygame.display.flip()
+
+running = True
+
+# window running loop
+while running:
+
+    for event in pygame.event.get():
+
+        # Check for QUIT event       
+        if event.type == pygame.QUIT: 
+            running = False
