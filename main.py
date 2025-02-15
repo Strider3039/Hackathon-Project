aabd1ba4e03127008a214736af98fb07623897ca
Strider20 <ethan.t.olsen@wsu.edu>
2025-02-15 13:42:26 -0800

    relative window size

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import turtle
+import tkinter as tk
 import pygame
 
+# gets the root to the tkinter library
+root = tk.Tk()
+
+# gets user system screen width and height
+screenWidth = root.winfo_screenwidth()
+screenHeight = root.winfo_screenheight()
 
 background_color = (169, 169, 169)
 
 # set screen size
-screen = pygame.display.set_mode((800, 800))
+screen = pygame.display.set_mode((screenWidth / 2, screenHeight / 2))
 
 #window name
 pygame.display.set_caption("Sticker Maker")
@@ -24,3 +30,7 @@ while running:
         # Check for QUIT event       
         if event.type == pygame.QUIT: 
             running = False
+        # key press processor
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:  # Directly check the key event
+                running = False
