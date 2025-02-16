5be4ed84112ccd6c9b5387903b25df67df2cba93
Aiden Vezzetti <115849073+AMVezz@users.noreply.github.com>
2025-02-15 21:12:00 -0800

    Update userInterface.py

--- userInterface.py
+++ userInterface.py
@@ -14,28 +14,7 @@ class State(Enum):
     MENU_STATE = "menuState"
     TRACE_STATE = "traceState"
 
-class StateManager:
-    
-    def __init__(self, screen):
-        self.states = {
-            State.MENU_STATE: MenuState(screen),
-            State.TRACE_STATE: TraceState(screen),
-        }
-        self.currentState = self.states[State.MENU_STATE]
-    
-    def change_state(self, new_state):
-        self.currentState = self.states[new_state]
-        
-    def handle_events(self, event):
-        self.currentState.handle_events(event, self)
-    
-    def update(self):
-        self.currentState.update()
-    
-    def draw(self, screen):
-        self.currentState.draw(screen)
-
-class MenuState:
+class Menu:
     
     def __init__(self, screen):
         self.screen = screen
@@ -48,9 +27,12 @@ class MenuState:
         self.exitButton = Button((self.screenWidth/2) - (BUTTON_WIDTH / 2), self.galleryButton.get_pos()[1] + BUTTON_PADDING, BUTTON_WIDTH, BUTTON_HEIGHT, BLUE, "Exit")
     
     def handle_events(self, event, state_manager):
-        if self.newStickerButton.check_click(event): state_manager.change_state(State.TRACE_STATE)
-        if self.galleryButton.check_click(event):    gallery.open_gallery()
-        if self.exitButton.check_click(event):       exit()
+        if self.newStickerButton.check_click(event):
+            pass
+        if self.galleryButton.check_click(event):
+            gallery.open_gallery()
+        if self.exitButton.check_click(event):
+            exit()
         
         
     def update(self):
@@ -61,17 +43,3 @@ class MenuState:
         self.newStickerButton.draw(screen)
         self.galleryButton.draw(screen)
         self.exitButton.draw(screen)
-
-class TraceState:
-    
-    def __init__(self, screen):
-        self.screen = screen
-    
-    def update(self):
-        pass
-    
-    def handle_events(self, event, state_manager):
-        pass
-
-    def draw(self, screen):
-        self.screen.fill(BACKGROUND_COLOR)
